689e9101f0678ade27ce76aef2ef176678af0802
Jyothi Chekka <jyothichekka08@gmail.com>
2026-08-16 19:42:06 +0530

    Fix production login API URL

--- frontend-cra/src/pages/Login.js
+++ frontend-cra/src/pages/Login.js
@@ -9,16 +9,19 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      });
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        }
+      );
 
       const data = await response.json();
 
